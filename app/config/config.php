2ae925ed44c6a418d533eb3871734c3d8114d1f2
setiawanalraz <alrazsetiawan79@gmail.com>
2022-11-30 16:20:58 +0700

    update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
 
 define('base_url', 'http://localhost/assasin_project/public');
 
-define('DB_HOST', '192.168.180.224');
-define('DB_USER', 'assasin');
-define('DB_PASS', 'b^=}mbY8wh1C');
+// define('DB_HOST', '192.168.180.224');
+// define('DB_USER', 'assasin');
+// define('DB_PASS', 'b^=}mbY8wh1C');
 
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
-// define('DB_PASS', '');
+define('DB_HOST', 'asasin1');
+define('DB_USER', 'root');
+define('DB_PASS', 'b^=}mbY8wh1C');
 
 define('DB_NAME', 'assasin_db');
 
